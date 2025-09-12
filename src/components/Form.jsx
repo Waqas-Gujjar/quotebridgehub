66a59d151f72b30189dd05918d234f0c8b1f5b83
WaqasYounas <wg4941792@gmail.com>
2025-09-13 02:54:21 +0500

    fff

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -107,20 +107,20 @@ export default function MultiStepQuoteForm() {
         trustedFormCertUrl,
       }).forEach(([key, value]) => formBody.append(key, value));
   
-      const scriptURL = "https://script.google.com/macros/s/AKfycbx4-CJVt6Rcx4PXCmCl5zZEcKy530z8c7BpaT8xUnNlI603eZNyZ9HDCqCvIEhF1ubLBw/exehttps://script.google.com/macros/s/AKfycbyK9153u7gFmmH5lR0XTOH9ZXnJxTRj_pWO3z7e9dQfmuZ4BLwbLvqz5B16IjnlVyFqPA/exec"; // Google Apps Script URL paste karo
+      const scriptURL = "https://script.google.com/macros/s/AKfycbx4-CJVt6Rcx4PXCmCl5zZEcKy530z8c7BpaT8xUnNlI603eZNyZ9HDCqCvIEhF1ubLBw/exehttps://script.google.com/macros/s/AKfycbyK9153u7gFmmH5lR0XTOH9ZXnJxTRj_pWO3z7e9dQfmuZ4BLwbLvqz5B16IjnlVyFqPA/exechttps://script.google.com/macros/library/d/1U9XZdMouwklx_0mfbS7sqelRdLSoFFhJwX9oeHRTpKBYnLWGX_T6XWaD/2https://script.google.com/macros/s/AKfycbyK9153u7gFmmH5lR0XTOH9ZXnJxTRj_pWO3z7e9dQfmuZ4BLwbLvqz5B16IjnlVyFqPA/execAKfycbz5f35yyqhONzw-WdEH5aWKqSL0WR26KpRKRmXS9-UzKrqaeT2Z1unodFzVvoWAMUavKg     "; // Google Apps Script URL paste karo
       const res = await fetch(scriptURL, {
         method: "POST",
         body: formBody,
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       });
   
       const result = await res.json();
       if (result.status === "ok") {
         navigate("/congratulations");
       } else {
-        setSubmitError("Google Sheet error: " + (result.message || "Unknown error"));
+        setSubmitError("Google Sheet error: " + (result.message || "Unknown error")); 
       }
     } catch (error) {
       console.error("Submission error:", error);
